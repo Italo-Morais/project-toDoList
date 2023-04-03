4907c6102c97513bcada1d3a796910e4dda09d1f
italo-morais <franciscoitalovianademorais@gmail.com>
2023-04-03 12:38:35 -0300

    add function onAddTask

--- to-do-list/src/components/Header/index.tsx
+++ to-do-list/src/components/Header/index.tsx
@@ -1,17 +1,44 @@
 import styles from "./Header.module.css";
 import logo from '../../assets/todo-logo.svg'
 import plus from '../../assets/plus.svg'
+import { useState, ChangeEvent, FormEvent } from 'react'
 
-export function Header() {
+interface HeaderProps {
+  onAddTask: (titleTasks: string) => void;
+}
+
+export function Header({onAddTask}: HeaderProps) {
+  const [titleTasks, setTitleTasks] = useState('')
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    onAddTask(titleTasks)
+    setTitleTasks('')
+  }
+
+  function handleChangeInput(event: ChangeEvent<HTMLInputElement>) {
+    setTitleTasks(event.target.value)
+    
+  }
+
   return (
     <>
       <div className={styles.app}>
         <img src={logo} alt="logo" />
       </div>
-      <div className={styles.newTask}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
-        <button><span>Criar</span><img src={plus} alt="" /></button>
-      </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className={styles.newTask}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            value={titleTasks}
+            onChange={handleChangeInput}
+          />
+          <button><span>Criar</span><img src={plus} alt="" /></button>
+        </div>
+      </form>
     </>
   );
 }
