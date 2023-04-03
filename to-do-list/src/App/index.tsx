b320ad45213a2afdc9d7932ea8e5946f2ec99c19
italo-morais <franciscoitalovianademorais@gmail.com>
2023-04-03 12:39:18 -0300

    add function deleteTasks

--- to-do-list/src/App/index.tsx
+++ to-do-list/src/App/index.tsx
@@ -2,27 +2,46 @@ import styles from "./App.module.css";
 import { Header } from "../components/Header";
 import { Tasks } from "../components/Tasks";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export interface ITasks {
-  id: number;
+  id: string;
   title: string;
   isCompleted: boolean;
 }
 
 export function App() {
-  const [tasks, setTasks] = useState<ITasks[]>([{
-    id: 1,
-    title: 'task',
-    isCompleted: false
-  }])
+  const [tasks, setTasks] = useState<ITasks[]>([])
 
+  function addTasks(titleTasks: string) {
+    if (titleTasks) {
+      setTasks([...tasks, {
+        id: uuidv4(),
+        title: titleTasks,
+        isCompleted: false
+      }])
+    } else (
+      alert('Digite uma nova tarefa')
+    )
+  }
+
+  function deleteTasks(taskID: string) {
+    const newTasks = tasks.filter(task => task.id !== taskID)
+    const isCertainty = confirm('Confirmar exclusão ?')
+
+    if (isCertainty) {
+      setTasks(newTasks)
+    } else {
+      return tasks
+    }
+  }
 
   return (
     <>
     <header>
-      <Header />
+      <Header onAddTask={addTasks} />
       <main>
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onDelete={deleteTasks}/>
       </main>
     </header>
     </>
