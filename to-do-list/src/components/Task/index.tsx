501a5af5465ff9231702649c92f5889ba6a61fb5
italo-morais <franciscoitalovianademorais@gmail.com>
2023-04-03 12:37:14 -0300

    add typing for onDelete

--- to-do-list/src/components/Task/index.tsx
+++ to-do-list/src/components/Task/index.tsx
@@ -1,24 +1,32 @@
-import styles from './Task.module.css'
-import trash from '../../assets/trash.svg'
+import styles from "./Task.module.css";
+import trash from "../../assets/trash.svg";
 import { BsFillCheckCircleFill } from "react-icons/bs";
+import { ITasks } from "../../App";
 
-export function Task() {
-    return(
-        <div className={styles.container}>
-            <button className={styles.checkContainer}>
-                {/* <div /> */}
-                 <BsFillCheckCircleFill /> 
-            </button>
+interface TaskProps {
+  task: ITasks;
+  onDelete: (taskID: string) => void;
+}
 
-            <div className={styles.taskTitle}>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vitae autem veniam cum repellat modi id corporis, fugiat facere, quibusdam facilis. At quasi suscipit assumenda unde molestias vero iusto voluptasasasasoakjsokaskoaoskkaosskoakososkasokaoksokasoksaoksaoskaoa Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla eveniet saepe dolores natus libero ullam minima explicabo est quos hic corrupti repellendus nemo dolore voluptas incidunt, error nam nobis numquam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab beatae pariatur autem, sequi iste dolores asperiores possimus eveniet. Quisquam deserunt optio voluptatem ducimus? Perspiciatis ipsam rerum eos sint, quos vitae?
-                </p>
-            </div>
+export function Task({task, onDelete}: TaskProps) {
+  return (
 
-            <button className={styles.deleteButton}>
-                <img src={trash} alt="icon" width={24} />
-            </button>
-        </div>
-    )
+    <div className={styles.container}>
+
+      <button 
+      className={styles.checkContainer}>
+        {task.isCompleted ? <BsFillCheckCircleFill /> : <div className={styles.check} /> }
+      </button>
+
+      <div className={styles.taskTitle}>
+        <p>{task.title}</p>
+      </div>
+
+      <button 
+      onClick={() => onDelete(task.id)}
+      className={styles.deleteButton}>
+        <img src={trash} alt="icon" width={24} />
+      </button>
+    </div>
+  );
 }
