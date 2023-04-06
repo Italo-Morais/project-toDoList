1741f551cf2630b6e5b50535b4c27937d8335fdf
italo-morais <franciscoitalovianademorais@gmail.com>
2023-04-05 23:25:40 -0300

    add events

--- to-do-list/src/components/Header/index.tsx
+++ to-do-list/src/components/Header/index.tsx
@@ -1,25 +1,24 @@
 import styles from "./Header.module.css";
-import logo from '../../assets/todo-logo.svg'
-import plus from '../../assets/plus.svg'
-import { useState, ChangeEvent, FormEvent } from 'react'
+import logo from "../../assets/todo-logo.svg";
+import plus from "../../assets/plus.svg";
+import { useState, ChangeEvent, FormEvent } from "react";
 
 interface HeaderProps {
   onAddTask: (titleTasks: string) => void;
 }
 
-export function Header({onAddTask}: HeaderProps) {
-  const [titleTasks, setTitleTasks] = useState('')
+export function Header({ onAddTask }: HeaderProps) {
+  const [titleTasks, setTitleTasks] = useState("");
 
   function handleSubmit(event: FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
 
-    onAddTask(titleTasks)
-    setTitleTasks('')
+    onAddTask(titleTasks);
+    setTitleTasks("");
   }
 
   function handleChangeInput(event: ChangeEvent<HTMLInputElement>) {
-    setTitleTasks(event.target.value)
-    
+    setTitleTasks(event.target.value);
   }
 
   return (
