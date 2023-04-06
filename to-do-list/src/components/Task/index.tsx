8d56aa862b822ca89a1126af486a2a7697b979a0
italo-morais <franciscoitalovianademorais@gmail.com>
2023-04-05 23:30:11 -0300

    add onClick

--- to-do-list/src/components/Task/index.tsx
+++ to-do-list/src/components/Task/index.tsx
@@ -6,20 +6,21 @@ import { ITasks } from "../../App";
 interface TaskProps {
   task: ITasks;
   onDelete: (taskID: string) => void;
+  onCompleted: (taskID: string) => void;
 }
 
-export function Task({task, onDelete}: TaskProps) {
+export function Task({task, onDelete, onCompleted}: TaskProps) {
   return (
 
     <div className={styles.container}>
 
       <button 
-      className={styles.checkContainer}>
+      className={styles.checkContainer} onClick={() => onCompleted(task.id)}>
         {task.isCompleted ? <BsFillCheckCircleFill /> : <div className={styles.check} /> }
       </button>
 
       <div className={styles.taskTitle}>
-        <p>{task.title}</p>
+        <p className={task.isCompleted ? styles.completed : ''}>{task.title}</p>
       </div>
 
       <button 
