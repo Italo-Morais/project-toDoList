ba9ff522856dd834663449ee374fd31880ff51d2
italo-morais <franciscoitalovianademorais@gmail.com>
2023-04-05 23:21:40 -0300

    add function isCompletedTask

--- to-do-list/src/App/index.tsx
+++ to-do-list/src/App/index.tsx
@@ -2,7 +2,7 @@ import styles from "./App.module.css";
 import { Header } from "../components/Header";
 import { Tasks } from "../components/Tasks";
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 export interface ITasks {
   id: string;
@@ -11,39 +11,57 @@ export interface ITasks {
 }
 
 export function App() {
-  const [tasks, setTasks] = useState<ITasks[]>([])
+  const [tasks, setTasks] = useState<ITasks[]>([]);
+
+  function isCompletedTask(taskID: string) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskID) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
 
   function addTasks(titleTasks: string) {
     if (titleTasks) {
-      setTasks([...tasks, {
-        id: uuidv4(),
-        title: titleTasks,
-        isCompleted: false
-      }])
-    } else (
-      alert('Digite uma nova tarefa')
-    )
+      setTasks([
+        ...tasks,
+        {
+          id: uuidv4(),
+          title: titleTasks,
+          isCompleted: false,
+        },
+      ]);
+    } else alert("Digite uma nova tarefa");
   }
 
   function deleteTasks(taskID: string) {
-    const newTasks = tasks.filter(task => task.id !== taskID)
-    const isCertainty = confirm('Confirmar exclusão ?')
+    const newTasks = tasks.filter((task) => task.id !== taskID);
+    const isCertainty = confirm("Confirmar exclusão ?");
 
     if (isCertainty) {
-      setTasks(newTasks)
+      setTasks(newTasks);
     } else {
-      return tasks
+      return tasks;
     }
   }
 
   return (
     <>
-    <header>
-      <Header onAddTask={addTasks} />
-      <main>
-        <Tasks tasks={tasks} onDelete={deleteTasks}/>
-      </main>
-    </header>
+      <header>
+        <Header onAddTask={addTasks} />
+          </header>
+        <main>
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTasks}
+            onCompleted={isCompletedTask}
+          />
+        </main>
     </>
   );
 }
